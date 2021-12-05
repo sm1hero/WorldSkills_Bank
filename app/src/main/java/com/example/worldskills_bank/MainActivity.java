c4576eded9445a56900b15b9f73f914238c87fc6
Andrei Smirnov <smirnovandryushnka01@gmail.com>
2021-12-05 17:11:18 +0300

    MainActivity add:
    valuesSetUp
    getCurrentDate

--- app/src/main/java/com/example/worldskills_bank/MainActivity.java
+++ app/src/main/java/com/example/worldskills_bank/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.worldskills_bank;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.launcher_main);
+
+        valuesSetUp();
+    }
+
+    private void valuesSetUp() {
+        getCurrentDate();
+    }
+
+    private void getCurrentDate() {
+        TextView textView = findViewById(R.id.launcherMainContentBox_2_currentDate);
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
+        String currentDate = sdf.format(new Date());
+        textView.setText(currentDate);
     }
 }
